58b8c2467866f447bcfb78de628f21b33fd8bbea
AmmarJamil23 <ammarjamil7000@gmail.com>
2025-10-01 19:01:20 +0500

    commit changes

--- DOM/script.js
+++ DOM/script.js
@@ -4,8 +4,18 @@
 // let para = document.getElementsByTagName("p")
 // console.dir(para)
 
-let firstelement = document.querySelector('p');
-console.dir(firstelement)
+// let firstelement = document.querySelector('p');
+// console.dir(firstelement)
 
-let allelement = document.querySelectorAll('p')
-console.dir(allelement)
+// let allelement = document.querySelectorAll('p')
+// console.dir(allelement)
+
+// let h2 = document.querySelector("h2");
+
+// console.dir(h2.innerText)
+
+// h2.innerText = h2.innerText + " this is user1 ok hope you saw that"
+
+let divs = document.querySelectorAll(".box");
+divs[0].innerText = "new text"
+console.dir(divs)

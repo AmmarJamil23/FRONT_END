6cbe86c327ef5c3bfb9b784d98c8bff49062346e
AmmarJamil23 <ammarjamil7000@gmail.com>
2025-10-01 18:45:38 +0500

    DOM manipulation

---
+++ DOM/script.js
@@ -0,0 +1,11 @@
+// let a = document.getElementsByClassName("heading")
+// console.dir(a);
+
+// let para = document.getElementsByTagName("p")
+// console.dir(para)
+
+let firstelement = document.querySelector('p');
+console.dir(firstelement)
+
+let allelement = document.querySelectorAll('p')
+console.dir(allelement)

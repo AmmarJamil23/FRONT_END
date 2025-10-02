af9b47d816636d5db44859e351b87b9f2e28c128
AmmarJamil23 <ammarjamil7000@gmail.com>
2025-10-02 10:46:56 +0500

    DOM practice 2

---
+++ DOM2/script.js
@@ -0,0 +1,52 @@
+// let a = document.querySelector("h1");
+// console.log(a);
+
+// let a = document.querySelector("h1");
+// a.innerHTML = "InnerHTML usage"
+
+// let b = document.querySelector("h1").innerHTML = "Again";
+
+// let a = document.querySelector("h1");
+// a.style.color = "yellow";
+// a.style.backgroundColor = "blue"
+// a.innerHTML = "Changed Text"
+
+// var a = document.querySelector("h1");
+
+// a.addEventListener("click" ,() => {
+//     a.innerHTML = "HAHAAHAHAHHHAHAHAHHAHAHA";
+//     a.style.color = "black"
+//     a.style.backgroundColor = "yellow"
+
+// });
+// a.addEventListener("click", () => {
+//     console.log("hey")
+// })
+// let a = document.querySelector(".bulb")
+// let b = document.querySelector('.on');
+// // let c = document.querySelector('.off');
+// let flag = 0;
+
+
+// b.addEventListener("click", () => {
+
+//     if(flag == 0){
+//     a.style.backgroundColor = "yellow"
+//     console.log("clicked")
+//     flag = 1
+//     }
+//     else {
+//     a.style.backgroundColor = "transparent"
+//     console.log("Again Clicked")
+//     flag = 0
+//     }
+// })
+// let h = document.querySelectorAll("h1")
+// // console.log(h);
+
+// h.forEach((e) => {
+//     console.log(e)
+
+// })
+let h =document.querySelector("h1");
+h.innerHTML = "Helloooo"
